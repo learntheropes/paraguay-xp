fc9de384773f942e614d98ab174647107b43145c
learntheropes <giovanni@learntheropes.xyz>
2023-07-24 08:39:16 +0200

    fix collection levels

--- components/area/areaLevels.vue
+++ components/area/areaLevels.vue
@@ -12,7 +12,9 @@ const store = useEscortsStore();
 const levels = store.levels;
 
 const escorts = Object.keys(store.list).reduce((obj, levelName) => {
-  obj[levelName] = (area == 'outcall-only') ? store.list[levelName].filter(escort => !escort.area) : store.list[levelName].filter(escort => escort.area === area);
+  obj[levelName] = (area == 'outcall-only')
+    ? store.list[levelName].filter(escort => !escort.area)
+    : store.list[levelName].filter(escort => escort.area === area);
   return obj;
 }, {});
 </script>
