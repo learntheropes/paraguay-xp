e1a4b34c140a11a2cacffbd4da92cb0d83a87ab8
learntheropes <giovanni@learntheropes.xyz>
2023-07-24 08:37:33 +0200

    fix collection levels

---
+++ components/collection/collectionLevels.vue
@@ -0,0 +1,36 @@
+<script setup>
+const {
+  levels,
+  escorts
+} = defineProps({
+  levels: {
+    type: Array,
+    required: true
+  },
+  escorts: {
+    type: Object,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div>
+    <div v-if="![].concat(escorts.diamond, escorts.palladium, escorts.platinum, escorts.esmerald, escorts.gold).length">{{ $t('index.noEscort') }}</div>
+    <div v-for="level of levels" :key="level.name" class="block">
+      <div v-if="escorts[level.name].length">
+        <h2 class="title is-5 is-capitalized">{{ $t('index.levelName', { level: $t(level.name) }) }} ({{ escorts[level.name].length }})</h2>
+        <p class="subtitle is-7">{{ level.range }} {{ $t('index.PYG') }}</p>
+        <div class="columns is-mobile is-multiline">
+          <div
+            v-for="escort of escorts[level.name]"
+            :key="escort.slug"
+            class="column is-half-mobile is-one-third-tablet is-one-quarter-desktop is-one-fifth-fullhd"
+          >
+            <CollectionCard :escort="escort" class="card-equal-height" />
+          </div>  
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- components/extra/extraLevels.vue
+++ components/extra/extraLevels.vue
@@ -18,22 +18,5 @@ const escorts = Object.keys(store.list).reduce((obj, levelName) => {
 </script>
 
 <template>
-  <div>
-    <div v-if="![].concat(escorts.diamond, escorts.palladium, escorts.platinum, escorts.esmerald, escorts.gold).length">{{ $t('index.noEscort') }}</div>
-    <div v-for="level of levels" :key="level.name" class="block">
-      <div v-if="escorts[level.name].length">
-        <h2 class="title is-5 is-capitalized">{{ $t('index.levelName', { level: $t(level.name) }) }} ({{ escorts[level.name].length }})</h2>
-        <p class="subtitle is-7">{{ level.range }} {{ $t('index.PYG') }}</p>
-        <div class="columns is-mobile is-multiline">
-          <div
-            v-for="escort of escorts[level.name]"
-            :key="escort.slug"
-            class="column is-half-mobile is-one-third-tablet is-one-quarter-desktop is-one-fifth-fullhd"
-          >
-            <CollectionCard :escort="escort" class="card-equal-height" />
-          </div>  
-        </div>
-      </div>
-    </div>
-  </div>
+  <CollectionLevels :levels="levels" :escorts="escorts" />
 </template>
