0ae888c1ff425b96d071fd790534f6a7e457ce30
learntheropes <giovanni@learntheropes.xyz>
2023-08-26 10:43:47 +0200

    add container to navbar and footer

--- components/layout/layoutFooter.vue
+++ components/layout/layoutFooter.vue
@@ -6,55 +6,57 @@
 
 <template>
   <footer class="footer">
-    <nav class="level">
-      <div class="level-left">
-        <div class="level-item">
-          <div class="buttons">
-            <OButton
-              :to="localePath('/legal')"
-              tag="router-link"
-              variant="primary"
-              inverted
-            >{{ $t('footer.legal') }}</OButton>
-            <OButton
-              :to="localePath('/news')"
-              tag="router-link"
-              variant="primary"
-              inverted
-            >{{ $t('footer.news') }}</OButton>
-            <OButton
-              :to="localePath('/blog')"
-              tag="router-link"
-              variant="primary"
-              inverted
-            >{{ $t('footer.blog') }}</OButton>
-            <OButton
-              :to="localePath('/admin')"
-              tag="router-link"
-              variant="primary"
-              inverted
-            >{{ $t('footer.admin') }}</OButton>
+    <div class="container">
+      <nav class="level">
+        <div class="level-left">
+          <div class="level-item">
+            <div class="buttons">
+              <OButton
+                :to="localePath('/legal')"
+                tag="router-link"
+                variant="primary"
+                inverted
+              >{{ $t('footer.legal') }}</OButton>
+              <OButton
+                :to="localePath('/news')"
+                tag="router-link"
+                variant="primary"
+                inverted
+              >{{ $t('footer.news') }}</OButton>
+              <OButton
+                :to="localePath('/blog')"
+                tag="router-link"
+                variant="primary"
+                inverted
+              >{{ $t('footer.blog') }}</OButton>
+              <!-- <OButton
+                :to="localePath('/admin')"
+                tag="router-link"
+                variant="primary"
+                inverted
+              >{{ $t('footer.admin') }}</OButton> -->
+            </div>
           </div>
         </div>
-      </div>
-      <div class="level-right">
-        <div class="level-item">
-          <div class="buttons">
-            <OButton
-              v-for="locale in availableLocales"
-              :key="locale.code"
-              tag="router-link"
-              :to="switchLocalePath(locale.code)"
-              variant="primary"
-              inverted
-            >{{ locale.name }}</OButton>
+        <div class="level-right">
+          <div class="level-item">
+            <div class="buttons">
+              <OButton
+                v-for="locale in availableLocales"
+                :key="locale.code"
+                tag="router-link"
+                :to="switchLocalePath(locale.code)"
+                variant="primary"
+                inverted
+              >{{ locale.name }}</OButton>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
-    <div class="level">
-      <div class="level-item has-text-centered">
-        © 2022 - 2050 {{ $t('footer.allRightReserved') }}
+      </nav>
+      <div class="level">
+        <div class="level-item has-text-centered">
+          © 2022 - 2050 {{ $t('footer.allRightReserved') }}
+        </div>
       </div>
     </div>
   </footer>
--- components/layout/layoutNavbar.vue
+++ components/layout/layoutNavbar.vue
@@ -29,61 +29,63 @@ const toggleFilter = (bool) => {
 
 <template>
   <nav class="navbar" role="navigation">
-  <div class="navbar-brand">
-    <div class="navbar-item">
-      <OButton
-        :to="localePath('/')"
-        tag="router-link"
-        variant="success"
-        inverted
-      >
-        <span class="has-text-weight-bold">ParaguayXP</span>
-      </OButton>
-    </div>
-  </div>
-  <div class="navbar-menu">
-    <div class="navbar-start">
-      <div v-if="isIndex" class="navbar-item">
-        <OIcon 
-          v-if="!showFilter"
-          @click.native="toggleFilter(true)"
-          icon="magnify"
-          variant="primary"
-        />
-        <OIcon 
-          v-else
-          @click.native="toggleFilter(false)"
-          icon="magnify-close"
-          variant="primary"
-        />
+    <div class="container">
+      <div class="navbar-brand">
+        <div class="navbar-item">
+          <OButton
+            :to="localePath('/')"
+            tag="router-link"
+            variant="success"
+            outlined
+          >
+            <span class="has-text-weight-bold">ParaguayXP</span>
+          </OButton>
+        </div>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <div v-if="isIndex" class="navbar-item">
+            <OIcon 
+              v-if="!showFilter"
+              @click.native="toggleFilter(true)"
+              icon="magnify"
+              variant="primary"
+            />
+            <OIcon 
+              v-else
+              @click.native="toggleFilter(false)"
+              icon="magnify-close"
+              variant="primary"
+            />
+          </div>
+        </div>
+        <div class="navbar-end">
+          <div v-if="status === 'authenticated'" class="navbar-item">
+            <OButton
+              :to="localePath('/dashboard')"
+              tag="router-link"
+              variant="primary"
+              inverted
+            >
+              <OIcon pack="mdi" icon="account-outline" />
+            </OButton>
+          </div>
+          <div v-if="status === 'authenticated'" class="navbar-item">
+            <OButton
+              @click.native="logOut"
+            >{{ $t('navbar.logout') }}</OButton>
+          </div>
+          <div v-else class="navbar-item">
+            <OButton
+              :to="localePath('/dashboard')"
+              tag="router-link"
+              variant="primary"
+            >{{ $t('navbar.publish') }}</OButton>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="navbar-end">
-      <div v-if="status === 'authenticated'" class="navbar-item">
-        <OButton
-          :to="localePath('/dashboard')"
-          tag="router-link"
-          variant="primary"
-          inverted
-        >
-          <OIcon pack="mdi" icon="account-outline" />
-        </OButton>
-      </div>
-      <div v-if="status === 'authenticated'" class="navbar-item">
-        <OButton
-          @click.native="logOut"
-        >{{ $t('navbar.logout') }}</OButton>
-      </div>
-      <div v-else class="navbar-item">
-        <OButton
-          :to="localePath('/dashboard')"
-          tag="router-link"
-          variant="primary"
-        >{{ $t('navbar.publish') }}</OButton>
-      </div>
-    </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <!-- <style scoped>
