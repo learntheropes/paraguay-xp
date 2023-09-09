3f088156210228d4393ab06e9a70fc37a9e2291f
learntheropes <giovanni@learntheropes.xyz>
2023-09-09 20:58:26 +0200

    fix table of content

--- components/blog/tableOfContent.vue
+++ components/blog/tableOfContent.vue
@@ -16,7 +16,7 @@ const {
       <li v-for="section of toc.links" :key="section.id" class="block">
         <NuxtLink :to="`#${section.id}`">{{ section.text }}</NuxtLink>
         <ul v-if="section.children">
-          <li v-for="children of section.children" :key="children.id">
+          <li v-for="children of section.children" :key="children.id" class="block">
             <span class="icon icon-link"></span>
             <NuxtLink :to="`#${children.id}`">{{ children.text }}</NuxtLink>
           </li>
