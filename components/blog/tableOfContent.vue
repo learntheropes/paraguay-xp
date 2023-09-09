1052a61f0840f5f0bea313bf509dce9d3ed338e8
learntheropes <giovanni@learntheropes.xyz>
2023-09-09 20:57:39 +0200

    fix table of content

--- components/blog/tableOfContent.vue
+++ components/blog/tableOfContent.vue
@@ -13,12 +13,12 @@ const {
   <div class="box">
     <div class="title is-5">{{ $t('blog.toc') }}</div>
     <ul>
-      <li v-for="section of toc.links" :key="section.id">
-        <OButton tag="router-link" :to="`#${section.id}`" variant="primary" inverted>{{ section.text }}</OButton>
+      <li v-for="section of toc.links" :key="section.id" class="block">
+        <NuxtLink :to="`#${section.id}`">{{ section.text }}</NuxtLink>
         <ul v-if="section.children">
           <li v-for="children of section.children" :key="children.id">
             <span class="icon icon-link"></span>
-            <OButton tag="router-link" :to="`#${children.id}`" variant="primary" inverted>{{ children.text }}</OButton>
+            <NuxtLink :to="`#${children.id}`">{{ children.text }}</NuxtLink>
           </li>
         </ul>
       </li>
